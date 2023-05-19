afa9112b3a9c6726e72da799dd6f355f447cb9e2
loc <>
2023-05-19 21:13:05 +0800

    add

--- DownTools/down.go
+++ DownTools/down.go
@@ -1,1 +1,43 @@
 package downtools
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type DownImage struct {
+	Wg *sync.WaitGroup
+}
+
+func (d DownImage) Down(url string) {
+	
+	re, _ := http.NewRequest("get", url, nil)
+	cli := &http.Client{}
+	res, err := cli.Do(re)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer res.Body.Close()
+	newFile(res.Body, "jpg")
+}
+
+/*
+imageType 为图片类型
+*/
+func newFile(r io.Reader, imageType string) {
+	//随机
+	rand.Seed(int64(time.Now().Nanosecond()))
+	randStr := strconv.Itoa(rand.Int())
+	f, err := os.Create("./Image/" + randStr + "." + imageType)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	io.Copy(f, r)
+}
